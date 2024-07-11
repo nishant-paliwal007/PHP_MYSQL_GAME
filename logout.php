565dcfe5dc636fbc573bd596c4cc95e48541e770
nishant <nishu.paliwal007@gmail.com>
2024-07-11 18:25:01 +0530

    add logout functionality

---
+++ logout.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+if (isset($_GET['logout'])) {
+    // Unset all of the session variables
+    $_SESSION = array();
+
+    // Destroy the session
+    session_destroy();
+
+    // Redirect to login page
+    header("Location: login.php");
+    exit();
+}
+?>
+
